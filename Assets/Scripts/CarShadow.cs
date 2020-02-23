3d7dd1961b153929aacca964dbeaba658e87d5f1
MurderousCrotch <1132464757@qq.com>
2020-02-23 15:15:57 +0900

    立项

---
+++ Assets/Scripts/CarShadow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarShadow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = transform.parent.position + Vector3.up * 5;
+        this.transform.eulerAngles = new Vector3(90, 0, 0);
+    }
+}
